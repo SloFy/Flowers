9f21ec8009b18af6cacd962e53d7539acef1b9c2
SloFy <hunter1623@mail.ru>
2014-09-23 23:50:07 +0400

    Добавилл проект

---
+++ Flowers/Flower/WebForm1.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Flower.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Введите логин<br />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Введите&nbsp; пароль<br />
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Введите имя<br />
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Введите фамилию<br />
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Введите почту<br />
+        <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Введите телефон<br />
+        <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Зарегистрироваться" />
+        <br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Flowers/Flower/WebForm1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Flower
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Data Source=ALEX-PC;Initial Catalog=Flower_DB;Integrated Security=True";
+                   
+    
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    int id = 1;
+                    connection.Open();
+                    string strSQL = "INSERT INTO Users VALUES ("+ id + ",'" 
+                        + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "','" + TextBox4.Text +
+                        "','" + TextBox5.Text + "'," + TextBox6.Text + ")";
+
+                
+                    SqlCommand command = new SqlCommand(strSQL, connection);
+                    command.ExecuteNonQuery();
+                  
+                }
+                catch (Exception ex)
+                {
+                   
+                }
+            }
+        }
+
+    }
+}
